72b444634c7d58a6df3402d0c35f42d3c1690668
ERAGON9 <liorbarak99@gmail.com>
2025-02-09 12:27:50 +0200

    Added matrixOfSymbol()

--- src/Main.java
+++ src/Main.java
@@ -42,7 +42,9 @@ public class Main
 
         //calculator();
 
-        numberGuessingGame();
+        //numberGuessingGame();
+
+        matrixOfSymbol();
     }
 
     public static void homeWorkAssignment1()
@@ -540,7 +542,36 @@ public class Main
         scanner.close();
     }
 
+    public static void matrixOfSymbol()
+    {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("\nMatrix of Symbol");
+        System.out.println("----------------\n");
+
+        int rows;
+        int cols;
+        char symbol;
+
+        System.out.print("Enter the number of rows: ");
+        rows = scanner.nextInt();
+        System.out.print("Enter the number of columns: ");
+        cols = scanner.nextInt();
+        System.out.print("Enter the symbol you want to use: ");
+        symbol = scanner.next().charAt(0);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                System.out.print(symbol + " ");
+            }
+
+            System.out.println();
+        }
+
+        scanner.close();
+    }
 
 
 
