18d6d6059435c765423bddc82174c9337c5f24c8
ERAGON9 <liorbarak99@gmail.com>
2025-02-09 13:51:33 +0200

    Added diceRollerProgram() Included: 1. printDie()

--- src/Main.java
+++ src/Main.java
@@ -46,7 +46,9 @@ public class Main
 
         //matrixOfSymbol();
 
-        bankingProgram();
+        //bankingProgram();
+
+        diceRollerProgram();
     }
 
     public static void homeWorkAssignment1()
@@ -657,8 +659,95 @@ public class Main
         return  amountToWithdraw;
     }
 
+    public static void diceRollerProgram()
+    {
+        Scanner scanner = new Scanner(System.in);
+        Random randomMachine = new Random();
+
+        int numberOfDice;
+        int total = 0;
+
+        System.out.println("\nDice Roller Program");
+        System.out.println("-------------------\n");
+
+        System.out.print("Enter the number of dice to roll: ");
+        numberOfDice = scanner.nextInt();
+
+        if (numberOfDice > 0)
+        {
+            for (int i = 0; i < numberOfDice; i++)
+            {
+                int roll = randomMachine.nextInt(1, 7);
+                System.out.println("You rolled: " + roll);
+                printDie(roll);
+                total += roll;
+            }
+            System.out.println("Total: " + total);
+        }
+        else
+        {
+            System.out.println("Number of dice must be greater than 0!");
+        }
 
 
+        scanner.close();
+    }
+
+    private  static void printDie(int roll)
+    {
+        String dice1 = """
+                 -------
+                |       |
+                |   ●   |
+                |       |
+                 -------
+                """;
+        String dice2 = """
+                 -------
+                | ●     |
+                |       |
+                |     ● |
+                 -------
+                """;
+        String dice3 = """
+                 -------
+                | ●     |
+                |   ●   |
+                |     ● |
+                 -------
+                """;
+        String dice4 = """
+                 -------
+                | ●   ● |
+                |       |
+                | ●   ● |
+                 -------
+                """;
+        String dice5 = """
+                 -------
+                | ●   ● |
+                |   ●   |
+                | ●   ● |
+                 -------
+                """;
+        String dice6 = """
+                 -------
+                | ●   ● |
+                | ●   ● |
+                | ●   ● |
+                 -------
+                """;
+        switch (roll)
+        {
+            case 1 -> System.out.println(dice1);
+            case 2 -> System.out.println(dice2);
+            case 3 -> System.out.println(dice3);
+            case 4 -> System.out.println(dice4);
+            case 5 -> System.out.println(dice5);
+            case 6 -> System.out.println(dice6);
+            default -> System.out.println("Invalid roll!");
+        }
+    }
 
 
 
